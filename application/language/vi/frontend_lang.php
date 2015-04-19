f9363f1722c7fc037ef8c0dd24f5cce9f2ea1734
huyvu0710 <hahuyvu0710@gmail.com>
2015-04-19 23:20:58 +0700

    Revert "update ngôn ngữ cái cuối"
    This reverts commit 9712180711f5599982da061eb9887e3c2842ae24.

--- application/language/vi/frontend_lang.php
+++ application/language/vi/frontend_lang.php
@@ -3,8 +3,4 @@
 $lang['ft_hello']			= "Xin chào";
 $lang['ft_login']			= "Đăng nhập";
 $lang['ft_find']			= "Tìm nào!";
-$lang['ft_menu_home']			= "Trang chủ";
-$lang['ft_menu_introduce']			= "Giới thiệu";
-$lang['ft_menu_agent']			= "Quản trị viên";
-$lang['ft_menu_contact']			= "Liên hệ";
 
