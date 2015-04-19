9712180711f5599982da061eb9887e3c2842ae24
huyvu0710 <hahuyvu0710@gmail.com>
2015-04-19 21:43:55 +0700

    update ngôn ngữ cái cuối

--- application/language/jp/frontend_lang.php
+++ application/language/jp/frontend_lang.php
@@ -3,3 +3,8 @@
 $lang['ft_hello']			= "こんにちは";
 $lang['ft_login']			= "ログイン";
 $lang['ft_find']			= "検索!";
+$lang['ft_menu_home']			= "ホーム";
+$lang['ft_menu_introduce']			= "紹介する";
+$lang['ft_menu_agent']			= "エージェント";
+$lang['ft_menu_contact']			= "接触";
+
