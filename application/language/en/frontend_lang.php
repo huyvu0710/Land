20a81f2b85445ccfaafc3fe2627ec2f7d267e3e5
huyvu0710 <hahuyvu0710@gmail.com>
2015-04-19 23:21:06 +0700

    Revert "Revert "update ngôn ngữ cái cuối""
    This reverts commit f9363f1722c7fc037ef8c0dd24f5cce9f2ea1734.

--- application/language/en/frontend_lang.php
+++ application/language/en/frontend_lang.php
@@ -3,4 +3,8 @@
 $lang['ft_hello']			= "Hello";
 $lang['ft_login']			= "Login";
 $lang['ft_find']			= "Search!";
+$lang['ft_menu_home']			= "Home";
+$lang['ft_menu_introduce']			= "Introduce";
+$lang['ft_menu_agent']			= "Agent";
+$lang['ft_menu_contact']			= "Contact";
 
