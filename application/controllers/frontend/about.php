92827157ca0fa4e459386bd66d01a6311392a133
huyvu0710 <hahuyvu0710@gmail.com>
2015-04-20 10:41:59 +0700

    dang ky
    them phan dang ky

--- application/controllers/frontend/about.php
+++ application/controllers/frontend/about.php
@@ -6,13 +6,27 @@ class About extends MY_Controller
     {
         parent::__construct();
         $this->my_layout->setLayout("layout/frontend");
+        $this->auth = $this->my_auth->check(); 
     }
 
     /*
     ******** About
     **********************************************/
-    public function index()
+    public function index($lang = '')
     {
+        if(empty($lang)){
+            $_lang = $this->session->userdata('_lang');
+            if (isset($_lang) && !empty($_lang) && $_lang != 'vi') {
+                $this->session->set_userdata('_lang', 'vi');
+            }
+        }
+        else{
+            if (!empty($lang) && in_array($lang, array('jp', 'en'))) {
+                $this->session->set_userdata('_lang', $lang);
+            }
+        }
+        $_lang = $this->session->userdata('_lang');
+        $this->lang->load('frontend', $_lang); // $this->lang->load('filename', 'language'); filename: frontend_lang
         $this->my_layout->view("frontend/about/index");
     }
 }
